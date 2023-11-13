1ed6c2257ee6b8072fb5058d219114fdf357350f
LinaCor <LinaAxx@yandex.ru>
2023-11-13 16:52:54 +0300

    fixed bugs

--- js/script.js
+++ js/script.js
@@ -7,14 +7,18 @@ function getRandomWord(arr) {
 
 let answerArray = [];
 let remainingLetters = word.length;
+let attemptCounter = 10;
 
 for (let i = 0; i < word.length; i++) {
   answerArray.push('_');
 }
-while (remainingLetters > 0) {
+
+while (remainingLetters > 0 && attemptCounter > 0) {
   alert(answerArray.join(" "));
   console.log(word)
   let guess = prompt('угадай или уходи');
+  attemptCounter--;
+  console.log(attemptCounter)
 
   if (guess === null) {
     break;
@@ -23,6 +27,9 @@ while (remainingLetters > 0) {
   } else {
     for (let j = 0; j < word.length; j++) {
       if (word[j] === guess) {
+        if (answerArray[j] == guess) {
+          break;
+        }
         answerArray[j] = guess;
         remainingLetters--;
       }
@@ -30,5 +37,10 @@ while (remainingLetters > 0) {
   }
 }
 
-alert(answerArray.join(" "));
-alert("Вы великолепны, отгадали слово " + word);
+
+if (attemptCounter == 0 && remainingLetters !== 0) {
+  alert('Кончились попытки');
+} else {
+  alert(answerArray.join(" "));
+  alert('Вы великолепны, отгадали слово ' + word);
+}
