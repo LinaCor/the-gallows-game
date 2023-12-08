104e25c0c714a5c4276fee80a4124eb92f44ca89
LinaCor <LinaAxx@yandex.ru>
2023-12-08 20:20:38 +0300

    small changes

--- js/script.js
+++ js/script.js
@@ -1,12 +1,29 @@
 const VALID_SYMBOLS = /^[а-я]$/i;
-const gameWords = ['небо', 'чебурек', 'илюша', 'релокация', 'отпуск', 'кейс', 'литература', 'орнитология', 'солнце', 'автомобиль', 'книга', 'кинология', 'хронометрия', 'вода', 'помело', 'часы'];
+const gameWords = [
+  'небо',
+  'чебурек',
+  'илюша',
+  'релокация',
+  'отпуск',
+  'кейс',
+  'литература',
+  'орнитология',
+  'солнце',
+  'автомобиль',
+  'книга',
+  'кинология',
+  'хронометрия',
+  'вода',
+  'помело',
+  'часы'
+];
 let word = getRandomWord(gameWords);
 let answerArray = setupAnswerArray(gameWords);
 let remainingLetters = word.length;
 let gameActive = document.querySelector('.game-input');
 let lettersInput = gameActive.querySelector('input');
-let buttonVerif = gameActive.querySelector('button');
-let valueInputValidate = document.querySelector('.game-input__validation');
+let buttonToVerif = gameActive.querySelector('button');
+let gameMessage = document.querySelector('.game-input__validation');
 let guessWord = document.querySelector('.game-result__word');
 let human = document.querySelector('.human');
 
@@ -22,8 +39,8 @@ function setupAnswerArray(arr) {
   return array;
 }
 
-function endGame() {
-  buttonVerif.style.display = 'none';
+const endGame = () => {
+  buttonToVerif.style.display = 'none';
   let btnRestartGame = document.createElement('button');
   btnRestartGame.classList.add('game-input__btn');
   btnRestartGame.textContent = 'сыграть ещё раз';
@@ -34,12 +51,12 @@ function endGame() {
 }
 
 
-function updateGameState(value, word, arr) {
+const updateGameState = (value, word, arr) => {
   if (!word.includes(value)) {
     let hiddenElements = human.querySelectorAll('.hidden');
     if (hiddenElements.length === 1) {
       hiddenElements[0].classList.remove('hidden');
-      valueInputValidate.innerHTML = 'попытки закончились :(';
+      gameMessage.innerHTML = 'попытки закончились :(';
       endGame();
     } else {
       hiddenElements[0].classList.remove('hidden');
@@ -58,58 +75,56 @@ function updateGameState(value, word, arr) {
   }
 
   if (!arr.some(elem => elem === '_')) {
-    valueInputValidate.innerHTML = `браво, вы отгадали слово "${word}"!`;
+    gameMessage.innerHTML = `браво, вы отгадали слово "${word}"!`;
     endGame();
   }
   guessWord.textContent = arr.join(' ');
 }
 
-
-window.addEventListener('load', () => {
-  guessWord.textContent = answerArray.join(' ');
-})
-
-
-buttonVerif.addEventListener('click', () => {
-  const valueInput = lettersInput.value;
-
+const validateInput = (valueInput) => {
   if (!valueInput.length) {
-    valueInputValidate.innerHTML = 'а как же выбрать букву?';
-    buttonVerif.disabled = true;
-    return;
+    return 'а как же выбрать букву?';
   }
 
   if (valueInput.length > 1) {
-    valueInputValidate.innerHTML = 'можно только одну букву';
-    buttonVerif.disabled = true;
-    return;
+    return 'можно только одну букву';
   }
 
   if (!VALID_SYMBOLS.test(valueInput)) {
-    valueInputValidate.innerHTML = 'только русские буквы и никаких цифр!';
-    buttonVerif.disabled = true;
+    return 'только русские буквы и никаких цифр!';
+  }
+
+  if (answerArray.includes(valueInput)) {
+    return 'эта буква уже была, попробуй другую';
+  }
+};
+
+window.addEventListener('load', () => {
+  guessWord.textContent = answerArray.join(' ');
+});
+
+buttonToVerif.addEventListener('click', () => {
+  const valueInput = lettersInput.value;
+  const errorMessage = validateInput(valueInput);
+
+  if (errorMessage) {
+    gameMessage.innerHTML = errorMessage;
+    buttonToVerif.disabled = true;
     return;
   }
 
-  if (answerArray.some((elem) => elem == valueInput)) {
-    valueInputValidate.innerHTML = 'эта буква уже была, попробуй другую';
-    buttonVerif.disabled = true;
-    return;
-  }
-
-  if (valueInput.length == 1 && VALID_SYMBOLS.test(valueInput)) {
-    valueInputValidate.innerHTML = '';
-    updateGameState(valueInput, word, answerArray);
-    lettersInput.value = '';
-  }
-})
+  gameMessage.innerHTML = '';
+  updateGameState(valueInput, word, answerArray);
+  lettersInput.value = '';
+}
+);
 
 lettersInput.addEventListener('input', () => {
   if (lettersInput.value.length == 1 || !lettersInput.value) {
-    buttonVerif.disabled = false;
-    valueInputValidate.innerHTML = '';
+    buttonToVerif.disabled = false;
+    gameMessage.innerHTML = '';
   }
-})
+});
 
 
 
